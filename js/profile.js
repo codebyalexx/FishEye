c5a35de72ae5aeca0e22cd3835fddeb79479793c
Alexandre Le Gal <justthewise@protonmail.com>
2022-01-03 16:51:50 +0100

    - Création du CSS pour le composant header photographe - Création du CSS pour la liste des médias & le filtre des médiaas - Prototype du dropdown - Affichage du profil dynamique vis à vis du JSON grâce à un script Javascript / TODO : Traiter les fichiers vidéos

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,79 @@
-(function () {
+const pName = document.querySelector("#auto-name");
+const caption = document.querySelector("#auto-caption");
+const tags = document.querySelector("#auto-tags");
+const image = document.querySelector("#auto-image");
+const medias = document.querySelector("#auto-medias");
 
+function getPagePhotographerId () {
+  let paramsString = window.location.href.split("?");
+  paramsString.shift();
+  paramsString = paramsString.join("?");
+
+  const searchParams = new URLSearchParams(paramsString);
+
+  return searchParams.get("photographerid").toString();
+}
+
+function mediaElementTemplate ({ title, likes, name, image }) {
+  const element = document.createElement("li");
+  element.className = "medias-item";
+
+  element.innerHTML = `<article class="medias-container">
+  <img
+    src="/img/Sample Photos/${name}/${image}"
+    alt="${title}"
+    class="medias-thumb"
+  />
+  <div class="medias-infos">
+    <p class="medias-infos-title">${title}</p>
+    <span class="medias-infos-likes">${likes} <i class="fas fa-heart"></i></span>
+  </div>
+</article>`;
+
+  return element;
+}
+
+(function () {
+  // eslint-disable-next-line no-undef
+  request("/data/FishEyeData.json", "GET")
+    .then((data) => {
+      try {
+        data = JSON.parse(data);
+
+        const targetProfile = data.photographers.find(
+          (p) => p.id.toString() === getPagePhotographerId()
+        );
+        const targetMedias = data.media.filter(function (el) {
+          return el.photographerId.toString() === getPagePhotographerId();
+        });
+
+        pName.innerText = targetProfile.name;
+        caption.innerHTML = `${targetProfile.city}, ${targetProfile.country} <span>${targetProfile.tagline}</span>`;
+        image.src = `/img/Sample Photos/Photographers ID Photos/${targetProfile.portrait}`;
+        targetProfile.tags.forEach((t) => {
+          const tagElement = document.createElement("li");
+          tagElement.innerHTML = `<span class="tag"
+    ><a href="#!" class="tag-text">#${t}</a></span
+  >`;
+
+          tags.appendChild(tagElement);
+        });
+
+        targetMedias.forEach((media) => {
+          medias.appendChild(mediaElementTemplate({
+            image: media.image,
+            likes: media.likes,
+            name: targetProfile.name,
+            title: media.title
+          }));
+        });
+      } catch (e) {
+        console.log(e);
+        alert("Une erreur est survenue lors du chargement du profil. Code 1");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Une erreur est survenue lors du chargement du profil. Code 2");
+    });
 })();
